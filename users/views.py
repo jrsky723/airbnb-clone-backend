fbe3c5c0509d2161198da59c205db5132e1d629e
hyunmo <jrsky723@gmail.com>
2023-09-12 13:26:39 +0900

    # 12.1 Create User
    - user를 만든는 API를 만들자
    - 비밀번호를 제외한 정보는 User로 부터 얻는다.
    - user가 제공하는 password는 hash해서 저장한다.
    - user의 set_password 함수를 이용하면, password를 hash해서 저장할 수 있다.
    - 이때 생성되는 user는 public user이므로 비밀번호를 테스트하려면, staff로 권한을 주고, admin 패널에서 로그인

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.exceptions import ParseError
 from rest_framework.permissions import IsAuthenticated
 from . import serializers
 
@@ -24,5 +25,21 @@ class Me(APIView):
             user = serializer.save()
             serializer = serializers.PrivateUserSerializer(user)
             return Response(data=serializer.data)
+        else:
+            return Response(data=serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError()
+        serializer = serializers.PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
         else:
             return Response(data=serializer.errors)
