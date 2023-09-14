f00805f55b28a72ab61242368a68d32b515fd336
hyunmo <jrsky723@gmail.com>
2023-09-14 19:43:56 +0900

    # 13.1
     - GET POST /experiences

--- experiences/models.py
+++ experiences/models.py
@@ -43,6 +43,16 @@ class Experience(CommonModel):
     def __str__(self) -> str:
         return self.name
 
+    def rating(experience):
+        count = experience.reviews.count()
+        if count == 0:
+            return 0
+        else:
+            total_rating = 0
+            for review in experience.reviews.all().values("rating"):
+                total_rating += review["rating"]
+            return round(total_rating / count, 2)
+
 
 class Perk(CommonModel):
 
