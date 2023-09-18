95884fc709412c9709364e280df46ee220447b02
hyunmo <jrsky723@gmail.com>
2023-09-18 18:54:50 +0900

    # 15.6 Environment Files
    - SECRET_KEY를 환경변수로 만들어서, 코드 외부에 저장하자.
    - .env 파일은 깃허브에 올리지 않는다.
    - .env를 읽는 패키지를 설치하자.
    
      - django-environ
    
    - django-third-party 패키지를 설치하자.
      - pipenv install django-environ
      - JWT 말고 더 나은 방법이 있다.

--- config/settings.py
+++ config/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^9%)ag*9i14ya!$ta04ay*2a%x$d8yy=21!i(b3o^nx@h(vb8p"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
